eacc37419893a1a67cf939713da357024a2421e3
Rajesh Pandian <rajesh.pandian@softrams.com>
2021-05-07 21:28:21 -0400

    Delete unused code

--- microservices/products/src/routes/index.route.js
+++
@@ -1,7 +0,0 @@
-const { Router } = require('express');
-const router = new Router();
-const productsRoutes = require('./products.route');
-
-router.use('/product', productsRoutes);
-
-module.exports = router;
