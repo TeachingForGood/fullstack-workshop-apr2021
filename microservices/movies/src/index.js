4109eb5a6ce3d15de42e56c7eb52238037b92163
Rajesh Pandian <rajesh.pandian@softrams.com>
2021-05-08 12:47:35 -0400

    Removed body parser

--- microservices/movies/src/index.js
+++ microservices/movies/src/index.js
@@ -1,11 +1,10 @@
-const bodyParser = require('body-parser');
 const env = require('dotenv');
 env.config();
 const routes = require('./routes/index.route');
 
 const express = require('express');
 const app = express();
-app.use(bodyParser.json());
+app.use(express.json());
 
 const port = process.env.PORT_MOVIES || 3000;
 
